57e6015371db884767f1a69c5f1d89c10f8a886e
spv <pvstrekalov@yandex.ru>
2022-11-26 00:17:11 +0400

    20-handle_errors

---
+++ 20-handle_errors/demo/app/Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var connectionString =
+                "Server=localhost;Database=WideWorldImporters;Integrated Security=true;";
+            try
+            {
+                using (var connection = new SqlConnection(connectionString))
+                using (var cmd = new SqlCommand("Warehouse.ChangeStockItemUnitPrice", connection))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // StockItemID 10 - существует, 1000 - НЕ существует
+                    cmd.Parameters.Add(new SqlParameter("@StockItemID", 1000));
+                    cmd.Parameters.Add(new SqlParameter("@UnitPrice", 99));
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                Console.WriteLine("OK");
+
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Код ошибки: " + ex.Number);
+            }
+        }
+    }
+}
